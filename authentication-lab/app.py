5dc34cf57d8938551a354b3393bcb14aad14dee0
bisharadi23-meet <109900012+bisharadi23-meet@users.noreply.github.com>
2022-07-31 14:32:17 +0300

    added everything

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,24 +2,58 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+firebaseConfig = {
+  "apiKey": "AIzaSyB1Kr9qRj5pbaQ_I5pN6j6_v_fHAy73PQs",
+  "authDomain": "firstbase-ea13c.firebaseapp.com",
+  "projectId": "firstbase-ea13c",
+  "storageBucket": "firstbase-ea13c.appspot.com",
+  "messagingSenderId": "461385299351",
+  "appId": "1:461385299351:web:913cea040ee6b63ab2c0d2",
+  "measurementId": "G-LQWGWSBHPY",
+  "databaseURL":""
+}
+
+firebase = pyrebase.initialize_app(firebadeConfig)
+auth = firebase.auth
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+	error = ""
+
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+			return redirect(url_for('add_tweet'))
+		except:
+			error = "Authentication failed"
+	return render_template("signin.html")
+
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+	if request.methods == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] =auth.create_user_with_email_and_password(email, password)
+			return redirect(url_for('add_tweet'))
+		except:
+		   error = "Authentication failed"
+   			return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    return render_template("add_tweet.html")
+	return render_template("add_tweet.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
